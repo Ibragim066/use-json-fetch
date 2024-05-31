446a86837cbf67e7691d2171deaab2515ab9ef7c
Ibragim <gamemega.ao@gmail.com>
2024-05-31 09:18:06 +0300

    implemented logic

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
 import "./App.css";
+import { DataFetchingComponent } from "./components";
+
+const App = () => (
+  <div className="container">
+    <DataFetchingComponent
+      url={process.env.REACT_APP_DATA_URL}
+      title="Successful Request"
+    />
+    <DataFetchingComponent
+      url={process.env.REACT_APP_ERROR_URL}
+      title="Error Request"
+      className="data-error"
+    />
+    <DataFetchingComponent
+      url={process.env.REACT_APP_LOADING_URL}
+      title="Loading Request"
+      className="data-loading"
+    />
+  </div>
+);
+
+export default App;
